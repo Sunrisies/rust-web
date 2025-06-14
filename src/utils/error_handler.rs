5e4583c67a659de7f7f528bda751c58743ddc98f
朱重迁 <z32664206862022@163.com>
2025-06-14 14:50:34 +0800

    refactor(error_handler): 简化错误处理逻辑
    移除了注释，使代码更加简洁。
    - 删除了关于从 `AppError` 中提取错误信息的注释。
    - 删除了关于根据错误类型设置 HTTP 状态码的注释。
    - 删除了关于构建 JSON 响应的注释。
    - 删除了关于替换原始响应的注释。
    - 删除了关于直接返回原始错误响应的注释。
    
    这些注释的移除并没有改变代码的功能，只是使代码看起来更干净。

--- src/utils/error_handler.rs
+++ src/utils/error_handler.rs
@@ -16,11 +16,9 @@ pub fn add_error_header<B: MessageBody + 'static>(
     if let Some(app_err) = req.extensions().get::<Rc<RefCell<Option<AppError>>>>() {
         let app_err = app_err.borrow();
         if let Some(app_error) = app_err.as_ref() {
-            // 从 AppError 中提取错误信息
             let error_message = app_error.to_string();
             error!("拦截到应用错误: {}", error_message);
 
-            // 根据错误类型设置 HTTP 状态码
             let status_code = match app_error {
                 AppError::Forbidden(_) => StatusCode::FORBIDDEN,
                 AppError::Unauthorized(_) => StatusCode::UNAUTHORIZED,
@@ -28,17 +26,14 @@ pub fn add_error_header<B: MessageBody + 'static>(
                 AppError::BadRequest(_) => StatusCode::BAD_REQUEST,
                 AppError::DeserializeError(_) => StatusCode::BAD_REQUEST,
                 AppError::Conflict(_) => StatusCode::CONFLICT,
-                // 其他错误类型处理
                 _ => StatusCode::INTERNAL_SERVER_ERROR,
             };
 
-            // 构建 JSON 响应
             let error_response = HttpResponse::build(status_code).json(json!({
                 "message": error_message,
                 "code": status_code.as_u16()
             }));
 
-            // 替换原始响应
             let new_res = ServiceResponse::new(req.clone(), error_response)
                 .map_into_boxed_body()
                 .map_into_right_body();
@@ -47,7 +42,6 @@ pub fn add_error_header<B: MessageBody + 'static>(
         }
     }
 
-    // 直接返回原始错误响应
     Ok(ErrorHandlerResponse::Response(
         ServiceResponse::new(req, _res)
             .map_into_boxed_body()
